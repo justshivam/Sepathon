f911b3d2e106b1c453309ce23869bcf71e9d6a79
Shivam Singh <stdnt.shivam@gmai.com>
2023-10-05 15:29:38 +0530

    Changes

--- sepathon-backend-flask/app.py
+++ sepathon-backend-flask/app.py
@@ -1,21 +1,27 @@
 from flask import Flask, request
 from website_footprint import calculateWebsiteFootprint
+from flask_cors import CORS, cross_origin
 import json
 
 app = Flask(__name__)
 BASE_URL = "/api/v1"
+cors = CORS(app)
 
 @app.route(BASE_URL)
+@cross_origin()
 def hello_world():
     return "Hello From Backend"
 
 @app.route(BASE_URL + '/networkTraffic', methods=['POST'])
+@cross_origin()
 def networkTraffic():
+    print('Test')
     data = json.loads(request.data.decode('utf-8'))
     url = data.get('url')
     data_used = 0
     if url is not None:
         data_used = calculateWebsiteFootprint(url)
+    print(data_used)
     return {
         'transfers': data_used
     }
